da28409c4f738c71deb74e0ef434d619edfec925
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-02-14 13:48:41 -0600

    update deps, use rattler-build

---
+++ recipe/run_test.py
@@ -0,0 +1,28 @@
+import sys
+from subprocess import call
+import shutil
+
+FAIL_UNDER = "49"
+COV = ["coverage"]
+RUN = ["run", "--source=safety", "--branch", "-m"]
+PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long"]
+REPORT = ["report", "--show-missing", "--skip-covered", f"--fail-under={FAIL_UNDER}"]
+
+SKIPS = [
+    "validate_with_basic_policy_file",
+    "debug_flag",
+    "generate_pass",
+]
+
+SKIP_OR = " or ".join(SKIPS)
+K = ["-k", f"not ({SKIP_OR})"]
+
+
+if __name__ == "__main__":
+    shutil.rmtree("tests/alerts")
+    sys.exit(
+        # run the tests
+        call([*COV, *RUN, *PYTEST, *K])
+        # maybe run coverage
+        or call([*COV, *REPORT])
+    )
